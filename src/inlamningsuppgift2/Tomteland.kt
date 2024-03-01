bcd2d5a5d28d088c2d6e169fc14446fa062587d3
Martin Sax <h.martin.sax@gmail.com>
2024-03-01 18:44:44 +0100

    initial commit

---
+++ src/inlamningsuppgift2/Tomteland.kt
@@ -0,0 +1,51 @@
+package inlamningsuppgift2
+
+class Tomteland {
+
+    val tomten = "Tomten"
+    val glader = "Glader"
+    val butter = "Butter"
+    val tröger = "Tröger"
+    val trötter = "Trötter"
+    val blyger = "Blyger"
+    val rådjuret = "Rådjuret"
+    val nyckelpigan = "Nyckelpigan"
+    val haren = "Haren"
+    val räven = "Räven"
+    val skumtomten = "Skumtomten"
+    val dammråttan = "Dammråttan"
+    val gråsuggan = "Gråsuggan"
+    val myran = "Myran"
+    val bladlusen = "Bladlusen"
+
+    val hierarkin = mapOf(
+        tomten to listOf(glader, butter),
+        glader to listOf(tröger, trötter, blyger),
+        butter to listOf(rådjuret, nyckelpigan, haren, räven),
+        trötter to listOf(skumtomten),
+        skumtomten to listOf(dammråttan),
+        räven to listOf(gråsuggan, myran),
+        myran to listOf(bladlusen)
+    )
+
+    fun getUnderlings(currentName: String, underlings: MutableList<String>): MutableList<String> {
+
+        tailrec fun addUnderlings(name: String, underlings: MutableList<String>, indexNr: Int): MutableList<String> {
+            hierarkin[name]?.forEach { t -> underlings.add(t) }
+
+            return if (indexNr == underlings.size) underlings
+            else addUnderlings(underlings[indexNr], underlings, indexNr + 1)
+        }
+
+        return addUnderlings(currentName, underlings, 0)
+
+    }
+
+    fun main() {
+
+        var list: MutableList<String> = mutableListOf()
+        println(getUnderlings("Räven", list))
+
+    }
+
+}
