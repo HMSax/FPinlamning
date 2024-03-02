452afcab0407dcc1c0c2ee06054f5043ce42409b
Martin Sax <h.martin.sax@gmail.com>
2024-03-02 10:36:03 +0100

    optimized code and got rid of trimmedList

--- src/inlamningsuppgift1/aoc2017day7.kt
+++ src/inlamningsuppgift1/aoc2017day7.kt
@@ -5,10 +5,15 @@ import java.io.File
 class aoc2017day7 {
 
     fun findBottomTower(towerList: List<String>): String {
-        val trimmedList = towerList.filter {t -> t.contains("->")}
-        val leftSideList = trimmedList.map{p -> p.substring(0, p.indexOf("(")-1)}
-        val rightSideList = trimmedList.map{z -> z.substring(z.indexOf(">")+2,z.lastIndex+1).split(", ")}.flatten()
+
+        val leftSideList = towerList.filter {t -> t.contains("->")}
+            .map{p -> p.substring(0, p.indexOf("(")-1)}
+
+        val rightSideList = towerList.filter {t -> t.contains("->")}
+            .map{z -> z.substring(z.indexOf(">")+2,z.lastIndex+1).split(", ")}.flatten()
+
         leftSideList.forEach { l -> if(!rightSideList.contains(l)) return(l) }
+
         return ""
     }
 }
