8a6fc8b0fc8660d94bbe45a81c1d47fc2bca9ef1
Martin Sax <h.martin.sax@gmail.com>
2024-03-04 00:11:46 +0100

    Added better solution for AoC 2017 day 2 part A and B

--- src/inlamningsuppgift1/aoc2017day2/aoc2017day2ver2.kt
+++ src/inlamningsuppgift1/aoc2017day2/aoc2017day2ver2.kt
@@ -3,23 +3,20 @@ package inlamningsuppgift1.aoc2017day2
 import java.io.File
 
 //this solution was inspired by https://todd.ginsberg.com/post/advent-of-code/2017/day2/
-class aoc2017day2ver2{
+class aoc2017day2ver2 {
 
     fun getChecksum(checksums: List<List<Int>>): Int =
-        checksums.sumOf{ it.max() - it.min() }
+        checksums.sumOf { it.max() - it.min() }
 
     fun getChecksum2(checksums: List<List<Int>>): Int {
-
-
         var sumByRow2 = mutableListOf<Int>()
-//        checksums.forEach {
-//            val splitRow = splitter.findAll(it).toList().map { it.value.toInt() }
-//            splitRow.forEach { t ->
-//                for (x in 0..<splitRow.size) {
-//                    if (t != splitRow[x] && t % splitRow[x] == 0) sumByRow2.add(t / splitRow[x])
-//                }
-//            }
-//        }
+        checksums.map {
+            it.forEach { t ->
+                for (x in 0..<it.size) {
+                    if (t != it[x] && t % it[x] == 0) sumByRow2.add(t / it[x])
+                }
+            }
+        }
         return sumByRow2.sum()
     }
 }
@@ -32,7 +29,8 @@ fun main() {
 
     val ingoingList = mutableListOf<List<Int>>()
 
-    spreadSheet.forEach { val myIntList = splitter.findAll(it).toList().map { it.value.toInt() }
+    spreadSheet.forEach {
+        val myIntList = splitter.findAll(it).toList().map { it.value.toInt() }
         println(myIntList)
         ingoingList.add(myIntList)
     }
