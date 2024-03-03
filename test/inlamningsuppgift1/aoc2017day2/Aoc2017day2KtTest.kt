ffcec0e7fefd287ba7b6fc9d14b5f351ea8b1599
Martin Sax <h.martin.sax@gmail.com>
2024-03-03 23:19:06 +0100

    Added solution for AoC 2017 day 2 part A and B

--- test/inlamningsuppgift1/aoc2017day2/Aoc2017day2KtTest.kt
+++ test/inlamningsuppgift1/aoc2017day2/Aoc2017day2KtTest.kt
@@ -2,15 +2,7 @@ package inlamningsuppgift1.aoc2017day2
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
-/*
-5 9 2 8
-9 4 7 3
-3 8 6 5
-In the first row, the only two numbers that evenly divide are 8 and 2; the result of this division is 4.
-In the second row, the two numbers are 9 and 3; the result is 3.
-In the third row, the result is 2.
-In this example, the sum of the results would be 4 + 3 + 2 = 9.
-* */
+
 class Aoc2017day2KtTest {
 
     val thistest = aoc2017day2()
