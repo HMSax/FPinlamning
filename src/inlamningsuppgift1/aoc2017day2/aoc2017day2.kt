514003e36496dd33e7e7d87ed0b61b129963e3d9
Martin Sax <h.martin.sax@gmail.com>
2024-03-03 23:18:22 +0100

    Added solution for AoC 2017 day 2 part A and B

--- src/inlamningsuppgift1/aoc2017day2/aoc2017day2.kt
+++ src/inlamningsuppgift1/aoc2017day2/aoc2017day2.kt
@@ -7,11 +7,25 @@ class aoc2017day2 {
     fun getChecksum(checksums: List<String>): Int {
         val splitter = """(\S+)""".toRegex()
         var sumByRow = mutableListOf<Int>()
-checksums.forEach {
-    val splitRow = splitter.findAll(it).toList().map {it.value.toInt()}
-    sumByRow.add(splitRow.max() - splitRow.min())
-}
+        checksums.forEach {
+            val splitRow = splitter.findAll(it).toList().map { it.value.toInt() }
+            sumByRow.add(splitRow.max() - splitRow.min())
+        }
         return sumByRow.sum()
+    }
+
+    fun getChecksum2(checksums: List<String>): Int {
+        val splitter = """(\S+)""".toRegex()
+        var sumByRow2 = mutableListOf<Int>()
+        checksums.forEach {
+            val splitRow = splitter.findAll(it).toList().map { it.value.toInt() }
+            splitRow.forEach { t ->
+                for (x in 0..<splitRow.size) {
+                    if (t != splitRow[x] && t % splitRow[x] == 0) sumByRow2.add(t / splitRow[x])
+                }
+            }
+        }
+        return sumByRow2.sum()
     }
 }
 
@@ -21,5 +35,6 @@ fun main() {
     val spreadSheet = File("src/inlamningsuppgift1/aoc2017day2/spreadSheetData.txt").readLines()
 
     println(upg4.getChecksum(spreadSheet))
+    println(upg4.getChecksum2(spreadSheet))
 
 }
--- test/inlamningsuppgift1/aoc2017day2/Aoc2017day2KtTest.kt
+++ test/inlamningsuppgift1/aoc2017day2/Aoc2017day2KtTest.kt
@@ -3,23 +3,30 @@ package inlamningsuppgift1.aoc2017day2
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 /*
-5 1 9 5
-7 5 3
-2 4 6 8
-The first row's largest and smallest values are 9 and 1, and their difference is 8.
-The second row's largest and smallest values are 7 and 3, and their difference is 4.
-The third row's difference is 6.
-In this example, the spreadsheet's checksum would be 8 + 4 + 6 = 18.
+5 9 2 8
+9 4 7 3
+3 8 6 5
+In the first row, the only two numbers that evenly divide are 8 and 2; the result of this division is 4.
+In the second row, the two numbers are 9 and 3; the result is 3.
+In the third row, the result is 2.
+In this example, the sum of the results would be 4 + 3 + 2 = 9.
 * */
 class Aoc2017day2KtTest {
 
     val thistest = aoc2017day2()
 
     val testsheet = listOf("5 1 9 5", "7 5 3","2 4 6 8")
+    val testsheet2 = listOf("5 9 2 8", "9 4 7 3","3 8 6 5")
     @Test
     fun getChecksumTest(){
         val testSum = thistest.getChecksum(testsheet)
         assertEquals(18, testSum)
+
+    }
+    @Test
+    fun getChecksum2Test(){
+        val testSum2 = thistest.getChecksum2(testsheet2)
+        assertEquals(9, testSum2)
 
     }
 
