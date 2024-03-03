f2a5e7ee4bff10fae6fc6d5408aad805b92f9cc3
Martin Sax <h.martin.sax@gmail.com>
2024-03-03 23:53:29 +0100

    Added better solution for AoC 2017 day 2 part A

---
+++ src/inlamningsuppgift1/aoc2017day2/aoc2017day2ver2.kt
@@ -0,0 +1,43 @@
+package inlamningsuppgift1.aoc2017day2
+
+import java.io.File
+
+//this solution was inspired by https://todd.ginsberg.com/post/advent-of-code/2017/day2/
+class aoc2017day2ver2{
+
+    fun getChecksum(checksums: List<List<Int>>): Int =
+        checksums.sumOf{ it.max() - it.min() }
+
+    fun getChecksum2(checksums: List<List<Int>>): Int {
+
+
+        var sumByRow2 = mutableListOf<Int>()
+//        checksums.forEach {
+//            val splitRow = splitter.findAll(it).toList().map { it.value.toInt() }
+//            splitRow.forEach { t ->
+//                for (x in 0..<splitRow.size) {
+//                    if (t != splitRow[x] && t % splitRow[x] == 0) sumByRow2.add(t / splitRow[x])
+//                }
+//            }
+//        }
+        return sumByRow2.sum()
+    }
+}
+
+fun main() {
+    val upg44 = aoc2017day2ver2()
+
+    val spreadSheet = File("src/inlamningsuppgift1/aoc2017day2/spreadSheetData.txt").readLines()
+    val splitter = """(\S+)""".toRegex()
+
+    val ingoingList = mutableListOf<List<Int>>()
+
+    spreadSheet.forEach { val myIntList = splitter.findAll(it).toList().map { it.value.toInt() }
+        println(myIntList)
+        ingoingList.add(myIntList)
+    }
+
+    println(upg44.getChecksum(ingoingList))
+    println(upg44.getChecksum2(ingoingList))
+
+}
